10e42278cb7bf7cb5aa60a53f9ad88910c2068e4
Tijmen Verhoef <tijmenmenno@gmail.com>
2025-08-27 09:56:18 +0200

    added score to score menu

---
+++ Assets/Scenes/UI/ScoreMenu/ScoreMenu.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class ScoreMenu : Control
+{ 
+    [Export(PropertyHint.File, "*.tscn,*.scn")]
+    public string MainMenuScenePath;
+
+    public override void _Ready()
+    {
+        Label timesCaught = GetNode<Label>("ColorRect/MarginContainer/VBoxContainer/HScoreContainer/ScoreLabels/TimesCaughtScore");
+        Label timesEscaped = GetNode<Label>("ColorRect/MarginContainer/VBoxContainer/HScoreContainer/ScoreLabels/TimesEscapedScore");
+        Label biscuitsCollected = GetNode<Label>("ColorRect/MarginContainer/VBoxContainer/HScoreContainer/ScoreLabels/BiscuitsCollectedScore");
+
+        timesCaught.Text = ScoreManager.Instance.PrevScore.TimesCaught.ToString();
+        timesEscaped.Text = "0";
+        biscuitsCollected.Text = ScoreManager.Instance.PrevScore.BiscuitsCollected.ToString();
+    }
+
+    public void OnQuitToMenuPressed()
+    {
+        GameManager.Instance.LoadScene(MainMenuScenePath);
+    }
+}
